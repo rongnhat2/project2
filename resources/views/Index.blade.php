f70d00c61df4c64487e60513d7594d082b512911
kirr <rongnhat2@gmail.com>
2019-01-24 15:34:25 +0700

    first commit

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NewsToday</title>
+	<!-- <link rel="stylesheet" href="/../public/css/style.css" /> -->
+	<link rel="stylesheet" href = "{{asset('css/style.css')}}">
+	<!-- <link rel="stylesheet" href = "{{asset('css/style.css')}}">  chạy được  -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="BarHeader">
+				
+			</div>
+			<div class="News">
+				<div class="row">
+					<div class="MainHeader col-md-4">
+						<div class="card">
+							<div class="zoombig">
+								<a class="zoom" href="#" target="_blank"><img class="image" src="{{asset('img/12.png')}}"></a>
+							</div>
+							<hr>
+						  	<div class="cont">
+						    	<a href="#"><i class="fas fa-arrow-circle-right"></i>Thủ tướng: Phải bỏ ngay quan điểm “quyền anh, quyền tôi”</a>
+						    	<h3>(Dân trí) - Làm việc với TP Hà Nội, Thủ tướng Nguyễn Xuân Phúc giao các bộ ngành khẩn trương xem xét từng việc trên nguyên tắc việc gì ai làm tốt hơn thì giao cho người đó, phải bỏ ngay quan điểm “quyền anh, quyền tôi”. </h3>
+						  	</div>
+						</div>
+					</div>
+					<div class="MidHeader col-md-4">
+						<div class="Top">
+							
+						</div>
+						<div class="Bot">
+							
+						</div>
+					</div>
+					<div class="ScrollHeader col-md-4">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="body">
+			
+		</div>
+
+		<div class="footer">
+			
+		</div>
+	</div>
+</body>
+</html>
